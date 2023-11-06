d388efe187d9bf059bd8d4201a5a450304f041e7
Hyoseo Lee <hyoseo837@gmail.com>
2023-11-06 10:56:46 -0500

    fix icon bug, remove unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import Connect4 from "./pages/Connect4";
 import Solitaire from "./pages/Solitaire";
+import SnakeGame from "./pages/SnakeGame";
 
 function App() {
     return (
         <div>
-            <DndProvider backend={HTML5Backend}>
-                <Solitaire />
-            </DndProvider>
+            <SnakeGame />
         </div>
     );
 }
