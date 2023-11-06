45166e1ebec783a9ef938ad6ed81965a3300c26b
Hyoseo Lee <hyoseo837@gmail.com>
2023-11-06 12:07:58 -0500

    fix manifest bug, remove konva

--- src/components/SnakeScreen.js
+++ src/components/SnakeScreen.js
@@ -1,30 +1,14 @@
 import React, { useEffect, useRef } from "react";
 
 const SnakeScreen = () => {
-    const canvasRef = useRef(null);
-
-    const draw = (ctx) => {
-        ctx.fillStyle = "#000000";
-        ctx.beginPath();
-        ctx.arc(100, 100, 20, 0, 2 * Math.PI);
-        ctx.fill();
+    const DrawHead = ({ direction }) => {
+        const dir = ["right", "up", "left", "down"].indexOf(direction);
+        return;
     };
-
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext("2d");
-
-        // first draw
-        context.fillStyle = "#dddddd";
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-
-        //Our draw come here
-        draw(context);
-    }, [draw]);
 
     return (
         <div>
-            <canvas ref={canvasRef}></canvas>
+            <canvas></canvas>
         </div>
     );
 };
